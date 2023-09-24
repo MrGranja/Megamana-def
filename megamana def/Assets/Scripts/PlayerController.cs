3e0e43b6e8eb71141de02d9aa132bfc81362c33e
MrGranja <croxzin@gmail.com>
2023-09-24 03:09:26 -0300

    sla faz o L
    tá andando bonito e pulando (só arrumar o timing deixa com o pai)

--- megamana def/Assets/Scripts/PlayerController.cs
+++ megamana def/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,15 @@ public class PlayerController : MonoBehaviour
 {
 
     Rigidbody2D rb2d;
-
+    private Animator anime;
+    [SerializeField]
+    private float rayLength;
+    [SerializeField]
+    private float rayPositionOffset;
+    Vector3 RayPositionCenter;
+    [SerializeField]
+    private LayerMask Ground;
+    private bool canJump;
    [SerializeField] float moveSpeed = 3f;
    [SerializeField] float jumpSpeed = 3f;
 
@@ -16,6 +24,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anime=GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,9 +33,41 @@ public class PlayerController : MonoBehaviour
         keyHorizontal = Input.GetAxisRaw("Horizontal");
         keyJump = Input.GetKeyDown(KeyCode.Space);
         rb2d.velocity = new Vector2(keyHorizontal * moveSpeed, rb2d.velocity.y);
-        if(keyJump)
+        if(keyJump && canJump)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+            canJump= false;
         }
     }
+
+    void FixedUpdate() {
+       Reverse();
+       anime.SetFloat("velocity", Mathf.Abs(keyHorizontal));
+       RayPositionCenter= transform.position;
+       float distance;
+
+       RaycastHit2D ray= Physics2D.Raycast(RayPositionCenter, Vector2.down,10,Ground);
+
+       distance= ray.distance;
+
+       anime.SetFloat("height", distance);
+       if(distance> 0.02f){
+       canJump= false;
+
+       }
+       if (distance < 0.02f) {
+              canJump=true;
+       }
+    } 
+
+    private void Reverse(){
+        if(keyHorizontal> 0){
+            transform.rotation= Quaternion.Euler(0,0,0);
+
+        }
+        if (keyHorizontal < 0) {
+            transform.rotation= Quaternion.Euler(0,180,0);
+        }
+    }
+
 }
